3af999d8e57933f8c3bca10a03953927aae8feff
Tomas Felcan <tomas.felcan@icloud.com>
2023-01-26 21:12:57 +0100

    test

--- two_sum/Solution.py
+++ two_sum/Solution.py
@@ -1,11 +1,11 @@
 class Solution(object):
-    nums = [3,3]
-    target = 6
-
+    nums = [4,2,3,5]
+    target = 8
+    print(nums[nums.index(2):].count(3))
+#todo : doesn't work when target is n and there is an n/2 element in the list
     def twoSum(self,nums, target):
         for f in nums:
-            nums.index(f) ==0
-            if (target - f) in nums:
+            if (target - f) in nums and nums[nums.index(f):].count(f)<2:
                 return(nums.index(f),nums.index((target - f) ))
                 break
 
